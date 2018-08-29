1be580cfc4e0204415c950eea3c65c42a2411279
Peter Kiss <ypetya@gmail.com>
2018-08-29 15:13:08 +0200

    EPMRPP-35551 add attachment code modal component

---
+++ app/src/pages/inside/logPage/modals/attachmentModal/README.md
@@ -0,0 +1,9 @@
+# Attachment modal windows
+
+## attachmentModal
+
+A basic attachment modal sceen
+
+## attachmentCodeModal
+
+A modal to display code with _highlight.js_.
--- app/src/pages/inside/logPage/modals/attachmentModal/attachmentModal.jsx
+++ app/src/pages/inside/logPage/modals/attachmentModal/attachmentModal.jsx
@@ -1,12 +1,9 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
 import { injectIntl, intlShape, defineMessages } from 'react-intl';
 import { reduxForm } from 'redux-form';
-import { ModalLayout, withModal, ModalField, ModalContentHeading } from 'components/main/modal';
-import styles from './launchAttachmentModal.scss';
-
-const cx = classNames.bind(styles);
+import { ModalLayout, withModal } from 'components/main/modal';
+import './attachmentModal.scss';
 
 const messages = defineMessages({
   title: {
@@ -47,7 +44,7 @@ export class AttachmentModal extends Component {
   };
 
   render() {
-    const { intl, handleSubmit, data } = this.props;
+    const { intl, handleSubmit } = this.props;
     const okButton = {
       text: intl.formatMessage(messages.close),
       onClick: (closeModal) => {
@@ -57,14 +54,7 @@ export class AttachmentModal extends Component {
 
     return (
       <ModalLayout title={intl.formatMessage(messages.title)} okButton={okButton}>
-        <form>
-          <ModalField>
-            <ModalContentHeading text={intl.formatMessage(messages.title)} />
-          </ModalField>
-          <ModalField>
-            <div className={cx('launch-name')}>{`${data.launch.name} #${data.launch.number}`}</div>
-          </ModalField>
-        </form>
+        <form />
       </ModalLayout>
     );
   }
